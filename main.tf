c5f6f37933a98793afa6f5b71ec244980712fb22
elsayed saper <elkot616@gmail.com>
2022-10-12 18:35:28 +0200

    initial commit

---
+++ main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+    region = "us-east-1"
+    access_key = "AKIAZY6XHON3HBN7BW7E"
+    secret_key = "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"
+  
+}
+resource "aws_vpc" "development-vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "dev-subnet-1" {
+    vpc_id = aws_vpc.development-vpc.id
+    cidr_block = "10.0.10.0/24"
+    availability_zone = "us-east-1a"
+
+}
